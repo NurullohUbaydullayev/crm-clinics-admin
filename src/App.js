deb75cfe6472402b2980fde7b2301360ff734bff
NurullohUbaydullayev <nurulloh.ubaydullayev2004@gmail.com>
2021-12-22 18:46:32 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
+import Login from "./Pages/Login/Login";
+import AdminUsers from "./Pages/AdminUsers/AdminUsers";
+import AdminOrders from "./Pages/AdminOrders/AdminOrders";
+import UserArchives from "./Pages/UserArchives/UserArchives";
+import Directions from "./Pages/Directions/Directions";
+
+// Components
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admins/users" element={<AdminUsers />} />
+          <Route path="/admins/users/:userId" element={<UserArchives />} />
+          <Route path="/admins/orders" element={<AdminOrders />} />
+          <Route path="/admins/directions" element={<Directions />} />
+        </Routes>
+      </main>
     </div>
   );
 }
